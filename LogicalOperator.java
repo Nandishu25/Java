4051b9422c0fced5769de070e734e6082ce62e22
Nandishu25 <126139331+Nandishu25@users.noreply.github.com>
2023-02-24 15:03:46 +0530

    Add files via upload
    New file

---
+++ LogicalOperator.java
@@ -0,0 +1,14 @@
+class LogicalOperator
+
+{
+    public static void main(String[] args) {
+        int a = 20;
+            int b = 50;
+            boolean res = (a<=b && b<=100);
+    
+            System.out.println(res);
+            System.out.println(a==b && b>=20);
+            System.out.println(a>=b  b>20);
+    }
+}
+
